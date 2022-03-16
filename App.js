ec29b1d0c0990c5c935591eb266f439e4e4f0bfe
Rui <ruimcpires@gmail.com>
2022-03-16 14:52:30 +0000

    Add SQLite

--- App.js
+++ App.js
@@ -4,6 +4,18 @@ import ReduxThunk from "redux-thunk";
 
 import PlacesNavigator from "./navigation/PlacesNavigator";
 import placesReducers from "./store/places-reducers";
+import { init } from "./helpers/db";
+
+init()
+  .then(() => {
+    console.log("Initialized database");
+  })
+  .catch((err) => {
+    console.log("Initializing db failed.");
+    console.log("====================================");
+    console.log(err);
+    console.log("====================================");
+  });
 
 const rootReducer = combineReducers({
   places: placesReducers,
